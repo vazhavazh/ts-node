d6c50aecbe5552433278a8e12366fb6abdf6e7a9
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-04-19 20:11:56 +0300

    finish

--- src/app/domain/person/Person.ts
+++ src/app/domain/person/Person.ts
@@ -1,14 +1,35 @@
 import { App } from "infra/App";
-import { Get, JsonController } from "routing-controllers";
+import { Body, Get, JsonController, Param, Post } from "routing-controllers";
+import PersonService from "./PersonService";
+import { IPerson } from "./PersonType";
 
 
-@JsonController()
+
+@JsonController("/person")
 export class Person {
 	public app = new App();
+	public service = new PersonService();
 
-	@Get("/api/person/getHello")
-	async getHello() {
-		return "Hello my friend=)";
+	// @Get("/getHello")
+	// async getHello() {
+	// 	return "Hello my friend=)";
+	// }
+
+	@Get()
+	async getAll() {
+		return this.service.getAll();
+	}
+
+	@Get('/:id')
+	async getById(@Param('id') id:number ) {
+		return this.service.getById(id);
+	}
+
+	@Post()
+	async setPerson(@Body() body: IPerson) {
+		await this.service.setPerson(body);
+
+		return "Person added";
 	}
 }
 
---
+++ src/app/domain/person/PersonService.ts
@@ -0,0 +1,23 @@
+import { IPerson } from "./PersonType";
+
+const storeData: IPerson[] = [];
+
+export default class PersonService {
+	async getAll(): Promise<IPerson[]> {
+		return storeData;
+	}
+
+	async getById(id: number): Promise<IPerson | {}> {
+		const person = storeData.find((el) => {
+			return el.id === id;
+		});
+
+		return person || {};
+	}
+
+	async setPerson(data: IPerson): Promise<true> {
+		storeData.push(data);
+
+		return true;
+	}
+}
